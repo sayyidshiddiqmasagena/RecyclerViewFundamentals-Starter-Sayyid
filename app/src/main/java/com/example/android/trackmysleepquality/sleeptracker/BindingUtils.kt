f86da18a53831d8c441332bfc9323414cebb68de
Sayyid Shiddiq Masagena <sayyidshiddiqm@gmail.com>
2021-12-09 22:56:14 +0800

    Codelab 2 Recycler View Grid Layout

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/BindingUtils.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/BindingUtils.kt
@@ -27,27 +27,27 @@ import com.example.android.trackmysleepquality.database.SleepNight
 
 @BindingAdapter("sleepDurationFormatted")
 fun TextView.setSleepDurationFormatted(item: SleepNight) {
-    text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, context.resources)
+        text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, context.resources)
 }
 
 
 @BindingAdapter("sleepQualityString")
 fun TextView.setSleepQualityString(item: SleepNight) {
-    text = convertNumericQualityToString(item.sleepQuality, context.resources)
+        text = convertNumericQualityToString(item.sleepQuality, context.resources)
 }
 
 
 @BindingAdapter("sleepImage")
 fun ImageView.setSleepImage(item: SleepNight) {
-    setImageResource(when (item.sleepQuality) {
-        0 -> R.drawable.ic_sleep_0
-        1 -> R.drawable.ic_sleep_1
-        2 -> R.drawable.ic_sleep_2
+        setImageResource(when (item.sleepQuality) {
+            0 -> R.drawable.ic_sleep_0
+            1 -> R.drawable.ic_sleep_1
+            2 -> R.drawable.ic_sleep_2
 
-        3 -> R.drawable.ic_sleep_3
+            3 -> R.drawable.ic_sleep_3
 
-        4 -> R.drawable.ic_sleep_4
-        5 -> R.drawable.ic_sleep_5
-        else -> R.drawable.ic_sleep_active
-    })
+            4 -> R.drawable.ic_sleep_4
+            5 -> R.drawable.ic_sleep_5
+            else -> R.drawable.ic_sleep_active
+        })
 }
